5dc6689bcf226699661110b00176a852a81bb935
HanDongWook <jja9312345@gmail.com>
2025-09-04 11:24:05 +0900

    add WebtoonModel

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,16 +1,22 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
-import 'package:webtoon_flutter/logger/logger.dart';
+import 'package:webtoon_flutter/models/webtoon_model.dart';
 
 class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstrances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      logger.i(response.body);
-      return;
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for (var webtoon in webtoons) {
+        webtoonInstrances.add(WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstrances;
     }
     throw Error();
   }
